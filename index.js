8cfd0c3e0559c2f838037036ae7169c34ec7607f
Hemadolase <hemadolase123@gmail.com>
2025-12-10 10:59:50 +0530

    Fix Server

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://its-power-frontend.vercel.app",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Multer
 const upload = multer({ dest: "uploads/" });
